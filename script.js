46c63b76dd7c535457ecc6047c296f0ff2db35fc
Eric Dai <ericdai123@gmail.com>
2024-06-24 13:54:35 -0500

    Added button hovering logic with realistic colors.

---
+++ script.js
@@ -0,0 +1,5 @@
+// global variables and constants
+const buttonContainer = document.querySelector('.button-container');
+const resultDisplay = document.querySelector('.result-display');
+
+let operator = '';
